46a87f4359af1918dec6572702f5496603e2eeb2
discountry <331027793@qq.com>
2018-02-09 01:42:08 +0800

    change config

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,13 +2,13 @@ const GitCoin = artifacts.require("./GitCoin.sol")
 const GitCoinCrowdsale = artifacts.require("./GitCoinCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
-  const ifSuccessfulSendTo = accounts[0]
-  const fundingGoalInEthers = 1000
-  const durationInMinutes = 360000
-  const etherCostOfEachToken = 0.01
+  const durationTime = 36000000
+  const rate = new web3.BigNumber(1000)
+  const wallet = accounts[0]
+  // const token = '0x00'
 
   deployer.deploy(GitCoin).then(function() {
-    return deployer.deploy(GitCoinCrowdsale, ifSuccessfulSendTo, fundingGoalInEthers, durationInMinutes, etherCostOfEachToken, GitCoin.address);
+    return deployer.deploy(GitCoinCrowdsale, durationTime, rate, wallet, GitCoin.address);
   });
 };
 
